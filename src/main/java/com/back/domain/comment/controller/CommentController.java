1e8e8a551340ca14fa574c6cf5a8096cc274ad19
Minji Kim <sml07113@gmail.com>
2025-12-05 14:17:51 +0900

    feat: 11강 - 댓글 삭제 api 구현

--- src/main/java/com/back/domain/comment/controller/CommentController.java
+++ src/main/java/com/back/domain/comment/controller/CommentController.java
@@ -4,6 +4,7 @@ import com.back.domain.comment.dto.CommentDto;
 import com.back.domain.comment.entity.Comment;
 import com.back.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class CommentController {
     private final PostService postService;
 
     @GetMapping
+    @Transactional(readOnly = true)
     public List<CommentDto> getCommentList(@PathVariable int postId) {
         List<Comment> comments = postService.findCommentsById(postId);
         return comments.stream()
@@ -26,9 +28,18 @@ public class CommentController {
     }
 
     @GetMapping("{id}")
+    @Transactional(readOnly = true)
     public CommentDto getComment(@PathVariable int postId,
                                  @PathVariable int id) {
         Comment comment = postService.findCommentByPostIdAndCommentId(postId, id);
         return new CommentDto(comment);
+    }
+
+    @GetMapping("{id}/delete")
+    @Transactional
+    public String delete(@PathVariable int postId,
+                         @PathVariable int id) {
+        postService.deleteComment(postId, id);
+        return "%d번 댓글이 삭제되었습니다.".formatted(id);
     }
 }
