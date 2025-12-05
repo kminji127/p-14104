57c44dd66fc8292f39412fd82e7591135bfc96b8
Minji Kim <sml07113@gmail.com>
2025-12-05 10:25:11 +0900

    refactor: 4강 - 공통 코드 처리

--- src/main/java/com/back/domain/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/controller/ApiV1PostController.java
@@ -3,19 +3,19 @@ package com.back.domain.post.controller;
 import com.back.domain.post.entity.Post;
 import com.back.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController // = @Controller + @ResponseBody
+@RequestMapping("/api/v1/posts") // 컨트롤러 공통 접두어
 @RequiredArgsConstructor
 public class ApiV1PostController {
     private final PostService postService;
 
-    @GetMapping("/api/v1/posts")
-    @ResponseBody
+    @GetMapping
     public List<Post> getItems() {
         return postService.findAll();
     }
