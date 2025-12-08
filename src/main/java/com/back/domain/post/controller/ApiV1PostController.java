9924fc2e538cab6094a64b5a225801a4dbf32d0b
Minji Kim <sml07113@gmail.com>
2025-12-08 11:30:59 +0900

    feat: ~24강 - 글 생성 기능 구현
    - POST 메소드
    - @RequestBody + 글 생성 전용 Request DTO(PostWriteRequest) 생성

--- src/main/java/com/back/domain/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/controller/ApiV1PostController.java
@@ -1,9 +1,11 @@
 package com.back.domain.post.controller;
 
 import com.back.domain.post.dto.PostDto;
+import com.back.domain.post.dto.PostWriteRequest;
 import com.back.domain.post.entity.Post;
 import com.back.domain.post.service.PostService;
 import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ApiV1PostController {
     private final PostService postService;
+
+    @PostMapping()
+    @Transactional
+    // @RequestBody: 클라이언트가 보낸 JSON 데이터를 Java 객체로 자동 매핑
+    public RsData<PostDto> writePost(@Valid @RequestBody PostWriteRequest postWriteRequest) {
+        Post createdPost = postService.write(postWriteRequest.title(), postWriteRequest.content());
+        return new RsData<>("200-1", "%d번 글이 생성되었습니다.".formatted(createdPost.getId()), new PostDto(createdPost));
+    }
 
     @GetMapping
     @Transactional(readOnly = true)
