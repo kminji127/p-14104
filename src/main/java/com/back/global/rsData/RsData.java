c702cb0f4131e59ad626d8da3f676d3051a18a7d
Minji Kim <sml07113@gmail.com>
2025-12-08 11:16:28 +0900

    feat: 19강 - 제너릭을 비우고 싶을 때 Void

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -5,4 +5,8 @@ public record RsData<T>(
         String msg,
         T data
 ) {
+    // RsData<Void>로 호출 가능
+    public RsData(String resultCode, String msg) {
+        this(resultCode, msg, null);
+    }
 }
